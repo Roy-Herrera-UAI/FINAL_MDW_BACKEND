5254a79d5a03551d9054806753d4f49ec7f3d30b
RoyHerrera <roy.herrera@alumnos.uai.edu.ar>
2026-03-22 17:44:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/health', (req, res) => {
+  res.json({ ok: true })
+})
+
+app.listen(3000)
